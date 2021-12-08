8fe3148f643ca54a1a8f96199a814840faaf296c
smuzoey <smuzoey@gmail.com>
2021-12-08 16:59:28 +0800

    delete instructionSet

--- redfish/processor.go
+++ redfish/processor.go
@@ -279,7 +279,6 @@ type Processor struct {
 	// InstructionSet shall contain the string which
 	// identifies the instruction set of the processor contained in this
 	// socket.
-
 	//InstructionSet InstructionSet
 	// Links is The Links property, as described by the Redfish
 	// Specification, shall contain references to resources that are related
